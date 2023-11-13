0e02b5df0bdc0919620ae210d76cfec650ef2d08
Francisco Morales <fjmoralesp@outlook.com>
2023-11-13 15:12:47 -0500

    add user on comments response

--- src/services/posts.service.js
+++ src/services/posts.service.js
@@ -1,12 +1,23 @@
 const Post = require('../models/post.model');
 const Comment = require('../models/comment.model');
+const User = require('../models/user.model');
 
 async function create(title, body, user) {
     return await Post.create({ title, body, UserId: user.id });
 }
 
 async function read() {
-    return await Post.findAll({ include: Comment });
+    return await Post.findAll({
+        include: [{
+            model: Comment,
+            as: 'Comments',
+            include: [{
+                model: User,
+                as: 'User',
+                attributes: ['id', 'username'],
+            }],
+        }]
+    });
 }
 
 async function update(postId, data) {
